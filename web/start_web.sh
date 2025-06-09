fbba0f69fdc0f750e2b9095e237d3308b18f6745
imenov06 <imenov06@gmail.com>
2025-06-09 20:19:30 +0300

    fix start_web.sh

--- web/start_web.sh
+++ web/start_web.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
+echo "Applying database migrations..."
 alembic upgrade head
+uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
 
